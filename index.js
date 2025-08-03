fc9b0c1e0d81025c661191a76ee1efed54e1a471
Hassaan Mehmood <hassaanmehmood68@gmail.com>
2025-08-04 01:52:58 +0500

    static patient data api

--- index.js
+++ index.js
@@ -38,6 +38,89 @@ app.get('/users', async (req, res) => {
   }
 });
 
+let patientData = {
+  "personalInfo": {
+    "regNo": 17631,
+    "dateReg": "2025-07-23",
+    "fullName": "Hassan Mehmood",
+    "dob": null,
+    "age": 20,
+    "education": "Bachelors",
+    "phone": "0330-7039460",
+    "mrNo": "1361252",
+    "profession": "Software Engineer",
+    "address": "Township",
+    "typeDm": 2,
+    "diagnosisDate": "2015-01-01",
+    "diagnosisAge": null,
+    "dateInsulinAdviced": null,
+    "dateInsulinStarted": null,
+    "menarche": null,
+    "menstrualCycle": null,
+    "menopause": null,
+    "gpa": null,
+    "familyHistory": null,
+    "notes": "patient is under a lot of stress due to academic and family burdens",
+    "insulinInstructions": null,
+    "prescriptionNotes": null,
+    "message1": null,
+    "message2": null,
+    "message3": null,
+    "alternateMedicine": null,
+    "nsaidUse": null,
+    "religion": null,
+    "ref": null,
+    "certificate": null,
+    "medicalAlerts": null,
+    "clinic": false,
+    "height": 5.3,
+    "nonPublic": null,
+    "idealWeight": "70-75",
+    "reportDate": null,
+    "discount": "0",
+    "lmp": null,
+    "edd": null,
+    "gestationalAgeByLmp": null,
+    "gestationalAgeByUs": null
+  },
+  "prescriptions": [],
+  "medicines": [
+    {
+      "id": 65481,
+      "regNo": 17631,
+      "medicine": 375,
+      "instruction": 53,
+      "dose": "3",
+      "duration": 1,
+      "dateStarted": "2025-07-23"
+    }
+  ],
+  "problems": [
+    {
+      "id": 33097,
+      "regNo": 17631,
+      "problem": 2,
+      "diagnosisDate": "2025-07-23",
+      "status": "Active",
+      "side": "N/A"
+    },
+    {
+      "id": 34107,
+      "regNo": 17631,
+      "problem": 2,
+      "diagnosisDate": "2025-08-01",
+      "status": "Unknown",
+      "side": "Right"
+    }
+  ],
+  "clinicalExams": [],
+  "labTests": []
+}
+
+app.get('/api/get-patient-data', (req, res) => {
+  res.json(patientData);
+})
+
 // EJS route: form to create user
 app.get('/users/new', (req, res) => {
   res.render('users/new');
