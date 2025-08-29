bdef22f265c4ff04d18cea6e69e6ae31292faf3a
Hassaan Mehmood <hassaanmehmood68@gmail.com>
2025-08-29 21:36:59 +0500

    semester page reworked

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ require('dotenv').config();
 const cors = require('cors')
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cors());
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
